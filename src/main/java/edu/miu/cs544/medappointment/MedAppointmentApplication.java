e84b0bb9cb0277a0de3bf0f1a2c27853509376ab
Murad Haciyev <murad.hajiyev@certoclav.com>
2020-05-11 11:59:08 -0500

    initial commit

---
+++ src/main/java/edu/miu/cs544/medappointment/MedAppointmentApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.medappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAppointmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedAppointmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs544/medappointment/MedAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.medappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAppointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
