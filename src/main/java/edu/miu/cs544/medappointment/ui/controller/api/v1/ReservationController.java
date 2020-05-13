862aeaba7f8d39a7b14f168ba048d336bf6c90e7
Hussien Elgabry <hussien.elgabry@live.com>
2020-05-13 11:09:20 -0500

    controller

--- src/main/java/edu/miu/cs544/medappointment/ui/controller/api/v1/ReservationController.java
+++ src/main/java/edu/miu/cs544/medappointment/ui/controller/api/v1/ReservationController.java
@@ -12,10 +12,15 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -38,4 +43,35 @@ public class ReservationController {
 
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
+    
+    @GetMapping("/{id}")
+	public ResponseEntity<ReservationResponseModel> getReservationById(@PathVariable long id) {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		ReservationDto reservation;
+		ReservationResponseModel response;
+		try 
+		{
+			reservation = reservationService.getReservationbyId(id);
+			response = mapper.map(reservation, ReservationResponseModel.class);
+		}
+		catch(Exception e)
+		{//No reservation with given id
+			response = null;
+			e.printStackTrace();
+		}
+		return new ResponseEntity(response, HttpStatus.CREATED);
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<ReservationResponseModel>> getAllReservation() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		List<ReservationDto> reservations = reservationService.getAllReservations();
+		List<ReservationResponseModel> response = reservations.stream()
+									.map(entity -> mapper.map(entity, ReservationResponseModel.class))
+									.collect(Collectors.toList());
+		return new ResponseEntity(response, HttpStatus.OK);
+	}
+    
 }
